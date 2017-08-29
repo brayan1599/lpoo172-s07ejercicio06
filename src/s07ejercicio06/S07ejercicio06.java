c28a94917a2edce56a305283b8bc617cba6f3179
alumno <alumno@LIMSFLAB06-16D.system-uap.adm>
2017-08-28 21:54:42 -0500

    first commit

---
+++ src/s07ejercicio06/S07ejercicio06.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s07ejercicio06;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class S07ejercicio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("ingrese lado de un hexagono: ");
+        double lado=input.nextDouble();
+        double area=((3*Math.pow(3, 0.5))/2)* Math.pow(lado, 2);
+        System.out.println("el area del hexagono es: " +area);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
